54726cce11c31cd6c05985b34f1e99bfedfe29d4
sunny5125 <sauravsunny05@gmail.com>
2017-03-01 19:33:13 +0530

    camera2 basic activity implemented

--- app/src/main/java/com/forkit/camsens/MainActivity.java
+++ app/src/main/java/com/forkit/camsens/MainActivity.java
@@ -1,22 +1,38 @@
+/*
+ * Copyright 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.forkit.camsens;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
-import com.forkit.camsens.sensors.Sensors;
 
-public class MainActivity extends AppCompatActivity {
-    Sensors s;
+import com.forkit.camsens.R;
+import com.forkit.camsens.camera.Camera2BasicFragment;
+
+public class MainActivity extends Activity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        s = new Sensors(this.getApplicationContext());
-    }
-    @Override
-    protected  void onDestroy(){
-        s.stopAll();
-        super.onDestroy();
-        getDelegate().onDestroy();
+        setContentView(R.layout.activity_camera);
+        if (null == savedInstanceState) {
+            getFragmentManager().beginTransaction()
+                    .replace(R.id.container, Camera2BasicFragment.newInstance())
+                    .commit();
+        }
     }
 
 }
